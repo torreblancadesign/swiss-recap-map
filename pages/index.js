1b8323bdf06b7e0fce5cceacc32e2356df8c532f
Enrique Torreblanca <enriquetchat@gmail.com>
2024-10-14 08:19:42 -0700

    File updated, no comment added

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-import React from 'react'; import Component from '../components/component'; const Index = () => {   return (     <>       <style>{`         body {           margin: 0;         }       `}</style>       <Component />     </>   ); }; export default Index;
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import Component from '../components/component';
+
+const Index = () => {
+  // Ensure modal works with Next.js
+  useEffect(() => {
+    Modal.setAppElement('body');  // Set this to 'body' since there's no #root
+  }, []);
+
+  return (
+    <>
+      <style>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
+      <Component />
+    </>
+  );
+};
+
+export default Index;
+ 
